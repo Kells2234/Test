8ef49ab4bbb85ef948bffbe24d390cb5f224464c
Chris Pleasants <cpleasants@microsoft.com>
2023-01-21 17:08:48 -0500

    added some functions to add, subtract, multiply, and divide

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+print("hello world.")
+
+# make a function that adds two numbers
+def add(a, b):
+    return a + b
+
+# make a function that subtracts two numbers
+def subtract(a, b):
+    return a - b
+
+# make a function that multiplies two numbers
+def multiply(a, b):
+    return a * b
+
+# make a function that divides two numbers
+def divide(a, b):
+    return a / b
+
+    
